202b57e9c0b231aa5d35b0f87b63b19e4dcc5dd3
Raj rana <raj.rana507@gmail.com>
2020-09-01 23:43:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Header from './header/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Router>
+        <div className="App">
+          <Switch>
+              <Route path="/checkout">
+                <div> this is check out page </div>
+              </Route>
+              <Route path="/login">
+                <div> this is login page </div>
+              </Route>
+              <Route path="/">
+                <div> this is home page </div>
+              </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <nav className="header">
+            <img src="/assets/images/Amazon-Logo.png" className="header__logo"/>
+        </nav>
+    )
+}
+
+export default Header
