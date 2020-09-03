cb4cc3169eeeb384054d405c6f0a0bc2ce5b08d5
Raj rana <raj.rana507@gmail.com>
2020-09-03 11:21:30 +0530

    completed with product component

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 import './Home.css';
 import '../product/Product'
 import Product from '../product/Product';
+
 function Home() {
     return (
         <div className="home">
             <img src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg" alt="" className="home__image" /> 
-            <Product 
-            id="123"
-            title="123"
-            image="123"
-            id="123"
-            />
+            <div className="home_row">
+                <Product 
+                id={123}
+                title={"(Renewed) Samsung Galaxy Note10 Lite (Aura Glow, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers"}
+                image={"https://images-na.ssl-images-amazon.com/images/I/51sRZk-7P2L._SL1000_.jpg"}
+                rating={5}
+                price={23}
+                />
+                <Product 
+                id={123}
+                title={"(Renewed) Samsung Galaxy Note10 Lite (Aura Glow, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers"}
+                image={"https://images-na.ssl-images-amazon.com/images/I/51sRZk-7P2L._SL1000_.jpg"}
+                rating={5}
+                price={23}
+                />
+            </div>
         </div>
     )
 }
--- src/product/Product.js
+++ src/product/Product.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
-function Product() {
+import './Product.css';
+import StarIcon from '@material-ui/icons/Star';
+function Product({title, price, rating, image}) {
     return (
-        <div>
-            
+        <div className="product">
+            <div className="product__info"> 
+                <p>{title}</p>
+                <p className="product__price">
+                    <small> Rs </small>
+                    <strong> { price } </strong>
+                </p>
+                <div className="product__rating">
+                    {
+                        Array(rating).fill().map((_)=><StarIcon className="product__star"/>)
+                    }
+                </div>
+            </div>  
+            <img src={image} alt="" />
+            <button> Add To basket </button>          
         </div>
     )
 }
